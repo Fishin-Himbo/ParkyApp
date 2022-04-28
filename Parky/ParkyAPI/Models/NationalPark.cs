2b5d36fed0607221d1c91c508a2adf2e26948eb8
Fishin-Himbo <jonathon.laurenza@gmail.com>
2022-04-28 14:46:34 -0400

    [INIT] Initial commit.  Contains a created Model.

---
+++ Parky/ParkyAPI/Models/NationalPark.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ParkyAPI.Models
+{
+    public class NationalPark
+    {
+        [Key] // Key attribute tells that this property should be a primary key
+        public int NationalParkId { get; set; }
+
+        [Required] // Required attribute tells that this property cannot be null.  The actual default value can be set in the migration file.
+        public string NationalParkName { get; set; }
+
+        [Required]
+        public int StateId { get; set; }
+
+        [Required]
+        public DateTime CreatedDtim { get; set; }
+
+        [Required]
+        public string CreatedUser { get; set; }
+
+        [Required]
+        public DateTime ModifiedDtim { get; set; }
+
+        [Required]
+        public DateTime ModifiedUser { get; set; }
+    }
+}
