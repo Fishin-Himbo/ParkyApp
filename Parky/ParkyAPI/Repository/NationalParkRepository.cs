1cb25ba95e59c6195533ee5b349c427c9af9e7db
Fishin-Himbo <jonathon.laurenza@gmail.com>
2022-04-29 15:10:29 -0400

    [INIT] Define NP repository methods.
    Also deleted State repository because that data will be manipulated through the database and not through the app.  Kept the DbSet because we still want the model to exist and for EF to create the database upon migration.

--- Parky/ParkyAPI/Repository/NationalParkRepository.cs
+++ Parky/ParkyAPI/Repository/NationalParkRepository.cs
@@ -1,49 +1,63 @@
-﻿using ParkyAPI.Models;
+﻿using ParkyAPI.Data;
+using ParkyAPI.Models;
 using ParkyAPI.Repository.IRepository;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ParkyAPI.Repository
 {
     public class NationalParkRepository : INationalParkRepository
     {
+        // DbContext field is required to access the database
+        private readonly AppDbContext _db;
+
+        // Constructor gets the context using dependency injection
+        public NationalParkRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public bool CreateNationalPark(NationalPark nationalPark)
         {
-            throw new System.NotImplementedException();
+            _db.NationalPark.Add(nationalPark);
+            return SaveNationalPark();
         }
 
         public bool DeleteNationalPark(NationalPark nationalPark)
         {
-            throw new System.NotImplementedException();
+            _db.NationalPark.Remove(nationalPark);
+            return SaveNationalPark();
         }
 
         public NationalPark GetNationalPark(int nationalParkId)
         {
-            throw new System.NotImplementedException();
+            return _db.NationalPark.FirstOrDefault(np => np.NationalParkId == nationalParkId);
         }
 
         public ICollection<NationalPark> GetNationalParks()
         {
-            throw new System.NotImplementedException();
+            return _db.NationalPark.OrderBy(np => np.NationalParkName).ToList();
         }
 
         public bool NationalParkExists(string nationalParkName)
         {
-            throw new System.NotImplementedException();
+            return _db.NationalPark.Any(np => np.NationalParkName.ToLower().Trim() == nationalParkName.ToLower().Trim());
         }
 
         public bool NationalParkExists(int nationalParkId)
         {
-            throw new System.NotImplementedException();
+            return _db.NationalPark.Any(np => np.NationalParkId == nationalParkId);
         }
 
         public bool SaveNationalPark()
         {
-            throw new System.NotImplementedException();
+            return _db.SaveChanges() >= 0; // SaveChanges returns an int - the # of rows written to the DB
         }
 
         public bool UpdateNationalPark(NationalPark nationalPark)
         {
-            throw new System.NotImplementedException();
+            _db.NationalPark.Update(nationalPark);
+            return SaveNationalPark();
         }
     }
 }
