39594505e829dabdbb2461c25bec65f47eb1a756
Fishin-Himbo <jonathon.laurenza@gmail.com>
2022-05-13 13:03:19 -0400

    [INIT] Register NationalPark with DI container

--- Parky/ParkyAPI/Startup.cs
+++ Parky/ParkyAPI/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using ParkyAPI.Data;
+using ParkyAPI.Repository;
+using ParkyAPI.Repository.IRepository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,6 +31,10 @@ namespace ParkyAPI
         {
             services.AddDbContext<AppDbContext>
                 (options => options.UseSqlServer(Configuration.GetConnectionString("ParkyConnection")));
+
+            // Registering classes to be used with Dependency Injection
+            services.AddScoped<INationalParkRepository, NationalParkRepository>();
+
             services.AddControllers();
         }
 
